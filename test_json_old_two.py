f6bb9894422343db7418e7c0d68886c394fac780
Carol Willing <carolcode@willingconsulting.com>
2014-08-07 14:32:07 -0700

    Add two more test examples

---
+++ test_json_old_two.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+import requests
+r = requests.post('http://httpbin.org/post', data=json.dumps({
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+))
+print r.status_code
+print r.json()
+print r.encoding
+print r.headers
